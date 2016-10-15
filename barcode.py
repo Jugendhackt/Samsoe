f214fe7b4f8e317c7e3039b6194729b73ba28241
Zozo2000 <zoes.email@yahoo.de>
2016-10-15 17:25:59 +0200

    Bild aufnehmen

--- barcode.py
+++ barcode.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python
 #Das ist die Decodierungssoftware fuer die QR-codes
+import subprocess 
 import qrtools
-
 import argparse
+
 
 parser = argparse.ArgumentParser(description='decode barcode')
 parser.add_argument('image')
 
 args = parser.parse_args()
 
+
+cmd = "raspistill -o {}".format(args.image)
+subprocess.call(cmd)
 
 qr = qrtools.QR()
 qr.decode(args.image)
