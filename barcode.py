69a29a66453c1eb3e4790c1e85ab5fc69ff6a2b5
Zozo2000 <zoes.email@yahoo.de>
2016-10-15 22:47:03 +0200

    gui

--- barcode.py
+++ barcode.py
@@ -3,7 +3,7 @@
 import subprocess 
 import qrtools
 import argparse
-
+from Tkinter import *
 
 parser = argparse.ArgumentParser(description='decode barcode')
 parser.add_argument('image')
@@ -11,22 +11,46 @@ parser.add_argument('--ip',default = "100.100.218.137:8080/samsoe/RPI.jsp")
 
 args = parser.parse_args()
 
-ean = "NULL"
+class Application(Frame):
+    def say_hi(self):
+        ean = "NULL"
 
-while ean == "NULL":
+        while ean == "NULL":
 
-    cmd = "raspistill -o {}".format(args.image)
-    subprocess.call(cmd, shell=True)
+            cmd = "raspistill -o {}".format(args.image)
+            subprocess.call(cmd, shell=True)
+
+            qr = qrtools.QR()
+            qr.decode(args.image)
+            print (qr.data)
+
+            ean = qr.data
+
+        co2 = subprocess.check_output('curl --silent --data "qrdata={}" {}'.format(ean,args.ip), shell=True)
+
+        print("CO2 emission: {}g".format(co2.strip()))
 
 
+    def createWidgets(self):
+        self.QUIT = Button(self)
+        self.QUIT["text"] = "QUIT"
+        self.QUIT["fg"]   = "red"
+        self.QUIT["command"] =  self.quit
 
+        self.QUIT.pack({"side": "left"})
 
-    qr = qrtools.QR()
-    qr.decode(args.image)
-    print (qr.data)
+        self.hi_there = Button(self)
+        self.hi_there["text"] = "Hello",
+        self.hi_there["command"] = self.say_hi
 
-    ean = qr.data
+        self.hi_there.pack({"side": "left"})
 
-co2 = subprocess.check_output('curl --silent --data "qrdata={}" {}'.format(ean,args.ip), shell=True)
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.createWidgets()
 
-print("CO2 emission: {}g".format(co2.strip()))
+root = Tk()
+app = Application(master=root)
+app.mainloop()
+root.destroy()
